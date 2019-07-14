25c1c420f6b80f8ac0aa28a4294c9fa7d4ea74d7
chicong250199 <maichicong1999@gmail.com>
2019-07-14 13:22:17 +0700

    version 6.1

--- app/main/skill/service.js
+++ app/main/skill/service.js
@@ -14,6 +14,7 @@ class SkillService extends BaseService {
         .join('skills', 'engineer_skill.skillId', 'skills.id')
         .groupBy('skillId', 'skills.name')
         .count('engineerId')
+        .orderBy('count', 'desc')
         .select('skillId', 'skills.name');
       let engineer = Models.Engineer.query()
         .whereNull('deletedAt')
